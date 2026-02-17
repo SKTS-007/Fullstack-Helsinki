09fbb595ddd59cb5921d3ba96166fbc0d3dc805e
SOORYA KIRAN T S <sooryakirants007@gmail.com>
2026-02-17 10:38:52 +0530

    .

--- Part 3/mongo.js
+++ Part 3/mongo.js
@@ -1,41 +1,45 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
-    console.log('invalid request')
+if (process.argv.length < 3) {
+    console.log('usage: node mongo.js <password> [name] [number]')
     process.exit(1)
 }
 
 const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-// gwHxC6yyacED8MjM
-const url =process.env.url
+const url = `mongodb+srv://sooryakiran:${password}@cluster0.wamnpaw.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    id: String,
     name: String,
-    number: String
+    number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    id: name + number,
-    name: name,
-    number: number
-})
-
-person.save().then(result => {
-    console.log(`added ${name} number ${number} to phonebook`)
-    mongoose.connection.close()
-})
-
-Person.find({}).then(result => {
-    result.forEach(person => {
-        console.log(person.name, person.number)
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
     })
+} else if (process.argv.length === 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Please provide both name and number to add: node mongo.js <password> <name> <number>')
     mongoose.connection.close()
-})
+}
